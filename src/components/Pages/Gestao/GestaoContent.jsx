0180e56ea1b8b83791ba11629a4143fb8b27a14e
Barbosa <henriqueeb@live.com>
2024-04-04 18:09:01 -0300

    feat( initial gestao and styled)

--- src/components/Pages/Gestao/GestaoContent.jsx
+++ src/components/Pages/Gestao/GestaoContent.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './GestaoContent.css';
 
 function GestaoContent() {
   return (
@@ -6,6 +7,14 @@ function GestaoContent() {
     <main className='GestaoContent'>
       <h2>Página de Gestão</h2>
       <p>Aqui você pode gerenciar suas operações.</p>
+
+      <div className='GestaoContainer'>
+        <div className='GestaoBox'>
+        <a href="">Análise de Dados e Relatórios</a>
+          <a href="">Gestão de Promoções e Descontos</a>
+          <a href="">Gestão Financeira e Contabilidade</a>
+        </div>
+      </div>
     </main>
     </>
   );
