cdc0a439c172264f4378e8dcc995375301d20d13
Barbosa <henriqueeb@live.com>
2024-04-08 18:04:33 -0300

    creat hooks useApi

--- src/components/Pages/MainContent/MainContent.jsx
+++ src/components/Pages/MainContent/MainContent.jsx
@@ -1,79 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './MainContent.css';
-import geladeiraBrastemp from '../../../assets/geladeira_brastemp.jpg'
+import API from '../../../hooks/useApi';
 
 function MainContent() {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const produtosData = await API.getProduto();
+
+        // Mapeia os produtos e faz uma nova solicitação para obter as imagens
+        const produtosComImagens = await Promise.all(produtosData.map(async produto => {
+          try {
+            const imagemData = await API.fetchData(`/api/imagem/${produto.id}`, 'GET');
+            // Verifica se existe uma imagem vinculada ao produto
+            const idImagem = imagemData ? imagemData.id : null;
+            // Adiciona o ID da imagem ao objeto do produto
+            return { ...produto, idImagem: idImagem };
+          } catch (error) {
+            console.error(`Erro ao buscar imagem do produto ${produto.id}:`, error);
+            return produto;
+          }
+        }));
+
+        setProdutos(produtosComImagens);
+      } catch (error) {
+        console.error('Erro ao buscar dados da API:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <main className="MainContent">
-      <h2>Bem-vindo ao Refrigera Plus!</h2>
-
-      <div className='ProductContainer'>
-        <div className='ProductBox'>
-          <div className='ImageContainer'>
-        <img src={geladeiraBrastemp} alt="Geladeira BrasTemp" />
-          </div>
-          <div className='ProductContent'>
-            <a href="">Geladeira BrasTemp 2024</a>
-            <div className='MindContainer'>
-              <div className='BrandContent'>
-              <a href="">BrasTemp</a>
+      <div className="ProductList">
+        <ul className="ProductItems">
+          {produtos.map(produto => (
+            <li key={produto.id} className="ProductItem">
+              {produto.idImagem && (
+                <div className='ProductImagem'>
+                  <img src={`http://45.235.53.125:8080/api/imagem/${produto.idImagem}`} alt="" className="ProductImage" />
+                </div>
+              )}
+              <div className="ProductInfo">
+                <div className='InfoNameCategory'>
+                  <h3>{produto.nome}</h3>
+                  <p className="ProductDetail">Categoria: {produto.categoria.nome}</p>
+                </div>
+                <div className='InfoPriceQuantity'>
+                  <p className="ProductDetail"> <span>R$</span>{produto.preco}</p>
+                  <p className="ProductDetail">Quantidade: {produto.quantidade}</p>
+                </div>
+                <div className='InfoDescription'>
+                  <p className="ProductDetail">Descrição: {produto.descricao}</p>
+                </div>
               </div>
-              <div className='InfoContent'>
-              <a href="" className='PriceContent'><small>R$</small> 1.989</a>
-              <a href="" className='DescricaoProduct'>Descição: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae vehicula nisl. Aenean ut condimentum ipsum. Cras a semper mi. Aenean luctus nibh eu lorem mattis, quis tincidunt diam consectetur. Nulla consectetur nibh ut neque efficitur dapibus. Mauris ultrices consequat vulputate. Vivamus condimentum sem vitae pharetra rutrum.</a>
-              </div>
-            </div>
-            <div className='LowContainer'>
-              <button>Comprar</button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className='ProductContainer'>
-        <div className='ProductBox'>
-          <div className='ImageContainer'>
-        <img src={geladeiraBrastemp} alt="Geladeira BrasTemp" />
-          </div>
-          <div className='ProductContent'>
-            <a href="">Geladeira BrasTemp 2024</a>
-            <div className='MindContainer'>
-              <div className='BrandContent'>
-              <a href="">BrasTemp</a>
-              </div>
-              <div className='InfoContent'>
-              <a href="" className='PriceContent'><small>R$</small> 1.989</a>
-              <a href="" className='DescricaoProduct'>Descição: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae vehicula nisl. Aenean ut condimentum ipsum. Cras a semper mi. Aenean luctus nibh eu lorem mattis, quis tincidunt diam consectetur. Nulla consectetur nibh ut neque efficitur dapibus. Mauris ultrices consequat vulputate. Vivamus condimentum sem vitae pharetra rutrum.</a>
-              </div>
-            </div>
-            <div className='LowContainer'>
-              <button>Comprar</button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className='ProductContainer'>
-        <div className='ProductBox'>
-          <div className='ImageContainer'>
-        <img src={geladeiraBrastemp} alt="Geladeira BrasTemp" />
-          </div>
-          <div className='ProductContent'>
-            <a href="">Geladeira BrasTemp 2024</a>
-            <div className='MindContainer'>
-              <div className='BrandContent'>
-              <a href="">BrasTemp</a>
-              </div>
-              <div className='InfoContent'>
-              <a href="" className='PriceContent'><small>R$</small> 1.989</a>
-              <a href="" className='DescricaoProduct'>Descição: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae vehicula nisl. Aenean ut condimentum ipsum. Cras a semper mi. Aenean luctus nibh eu lorem mattis, quis tincidunt diam consectetur. Nulla consectetur nibh ut neque efficitur dapibus. Mauris ultrices consequat vulputate. Vivamus condimentum sem vitae pharetra rutrum.</a>
-              </div>
-            </div>
-            <div className='LowContainer'>
-              <button className='BtnContent'>Comprarr</button>
-            </div>
-          </div>
-        </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
